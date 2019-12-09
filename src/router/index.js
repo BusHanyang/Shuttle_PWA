1efae9c7b103a359d61d2e8cdb50341e3f96d2e1
Taewan-P <swimingtw@gmail.com>
2019-12-09 18:35:11 +0900

    Remove unused module

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import Vue from "vue";
-import VueCookies from "vue-cookies";
 import Router from "vue-router";
 
 import Home from "@/components/Home";
@@ -7,7 +6,6 @@ import Box from "@/components/Box";
 import Title from "@/components/Title";
 
 Vue.use(Router);
-Vue.use(VueCookies);
 
 export default new Router({
   routes: [
