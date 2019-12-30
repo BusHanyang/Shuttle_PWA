905981d7367c388163d81993ccc903ce9f73c92f
HyeongChang Lee <hc9904@hanyang.ac.kr>
2019-12-30 22:04:57 +0900

    Fix sync error (#21)
    * Fix sync error

--- src/components/OneText.vue
+++ src/components/OneText.vue
@@ -1,23 +1,29 @@
 <template>
-  <div v-on:click="toggle = !toggle" :class="{ align_top: !toggle && this.buslst[1].type != 'F'}" class="time_box">
+  <div
+    v-on:click="toggle = !toggle"
+    :class="{ align_top: !toggle && this.buslst[1].type != 'F'}"
+    class="time_box"
+  >
     <bigname :val="this.where" class="bigName"></bigname>
     <div class="timetable">
       <span class="default-target">
-        <target :buslist="this.buslst" :a="0" class="target"></target>
+        <target :where="this.where" :buslist="this.buslst" :a="0" class="target"></target>
       </span>
       <span v-if="done" class="more" :class="{'invisible':toggle}">
-        <target :buslist="this.buslst" :a="1" class="target"></target>
-        <target :buslist="this.buslst" :a="2" class="target"></target>
-        <target :buslist="this.buslst" :a="3" class="target"></target>
-        <target :buslist="this.buslst" :a="4" class="target"></target>
+        <target :where="this.where" :buslist="this.buslst" :a="1" class="target"></target>
+        <target :where="this.where" :buslist="this.buslst" :a="2" class="target"></target>
+        <target :where="this.where" :buslist="this.buslst" :a="3" class="target"></target>
+        <target :where="this.where" :buslist="this.buslst" :a="4" class="target"></target>
       </span>
     </div>
   </div>
 </template>
 
 <script>
+import { EventBus } from "../event-bus";
 import BigName from "./BigName";
 import Target from "./Target";
+
 export default {
   name: "OneText",
   components: {
@@ -29,12 +35,17 @@ export default {
     return {
       dest: this.where,
       today: new Date(),
-      buslst: "[{time:"+new Date().getTime()+", type:}]",
+      buslst: "[{time:" + new Date().getTime() + ", type:}]",
       toggle: true,
       done: true
     };
   },
   created() {
+    EventBus.$on("imdone" + this.where, wru => {
+      this.buslst.shift();
+      let tempstr = { time: 0, type: "F" };
+      this.buslst.push(tempstr);
+    });
     this.parseBusList(this.where, this.today);
   },
 
@@ -67,15 +78,17 @@ export default {
                 parseInt(min) >
               0
             ) {
-              let tmpdate = Math.floor(new Date(year, month-1, day, temp[0], temp[1], sec) / 1000)
-              tempstr = {time: tmpdate, type: res[i].type}
+              let tmpdate = Math.floor(
+                new Date(year, month - 1, day, temp[0], temp[1], sec) / 1000
+              );
+              tempstr = { time: tmpdate, type: res[i].type };
               buslst.push(tempstr);
             }
           }
           if (buslst.length < 5) {
-            while(buslst.length < 5) {
-              tempstr = {time: 0, type: "F"}
-              buslst.push(tempstr)
+            while (buslst.length < 5) {
+              tempstr = { time: 0, type: "F" };
+              buslst.push(tempstr);
             }
           }
           this.buslst = buslst;
@@ -84,36 +97,35 @@ export default {
           console.log(e);
         });
     }
-  },
+  }
 };
 </script>
 
 <style scoped>
-  .time_box {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-around;
-  }
-  .align_top {
-    align-items: flex-start;
-  }
-
-  .timetable {
-    flex-direction: column;
-  }
-  .more {
-    text-align: right;
-    /*float: right;*/
-  }
-  .bigName {
-    margin-left: 1rem;
-    width: 30%;
-    text-align: left;
-    word-break: keep-all;
-  }
-  .invisible {
-  display: none;
+.time_box {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+.align_top {
+  align-items: flex-start;
 }
 
+.timetable {
+  flex-direction: column;
+}
+.more {
+  text-align: right;
+  /*float: right;*/
+}
+.bigName {
+  margin-left: 1rem;
+  width: 30%;
+  text-align: left;
+  word-break: keep-all;
+}
+.invisible {
+  display: none;
+}
 </style>
