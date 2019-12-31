2b25c3cf15272d637f5cdd92de5faa9a82838a3f
mjkya <mjkya@naver.com>
2020-01-01 02:47:51 +0900

    added happy 2020 banner
    successfully added banner, but failing to add image

---
+++ src/components/Banner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="banner">
+    <span class="mode-text"> <img src="src/assets/images/Hanyang_newyear.png"> HAPPY 2020 </span>
+  </div>
+</template>
+
+<style lang="css">
+.banner {
+  /*1rem == 14px*/
+  list-style-type: none;
+  padding: 0.357rem;
+  border: 0 white;
+  border-radius: 0.571rem;
+  box-shadow:0 0 2.142rem 0.142rem rgba(0, 0, 0, 0.15);
+  margin: 0 0.357rem 1.071rem;
+}
+</style>
