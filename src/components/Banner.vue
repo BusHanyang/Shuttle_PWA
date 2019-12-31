1446b0ca717da31dc707d768301fe3bc99331764
mjkya <mjkya@naver.com>
2020-01-01 03:36:59 +0900

    added newyear img

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="banner">
-    <span class="mode-text"> <img src="src/assets/images/Hanyang_newyear.png"> HAPPY 2020 </span>
+    <span class="mode-text"> <img id="newyear" src= "../assets/images/Hanyang_newyear.png"> HAPPY 2020 </span>
   </div>
 </template>
-
 <style lang="css">
 .banner {
   /*1rem == 14px*/
-  list-style-type: none;
+  list-style-type: none;3
   padding: 0.357rem;
   border: 0 white;
   border-radius: 0.571rem;
   box-shadow:0 0 2.142rem 0.142rem rgba(0, 0, 0, 0.15);
   margin: 0 0.357rem 1.071rem;
+}
+#newyear{
+  width: 3rem;
+  height: 3rem;
+  
 }
 </style>
