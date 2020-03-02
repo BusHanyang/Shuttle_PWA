70219ecbabc6d01c36e1791f4cd2f792c40df16e
mjkya <mjkya@naver.com>
2020-03-03 01:32:47 +0900

    added css to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,15 @@
 <template>
-  <footer>우리는 생각한다 무엇을? 디자인을</footer>
+  <footer class="footer">우리는 생각한다 무엇을? 디자인을. 글을 좀 더 써볼게요 커피 사주세요</footer>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="css" scoped>
+<style lang="css">
+.footer {
+  margin-top: 25px;
+  font-size: 10px;
+}
+
 </style>
