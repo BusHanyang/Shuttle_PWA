b3cfcada4cfc653a3d3c31052529a15b1a02174e
Yeonggyun.Kim <kygha7205@naver.com>
2020-03-10 00:57:34 +0900

    Delete unused assets

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -7,26 +7,6 @@ export default {};
 </script>
 
 <style lang="css" scoped>
-/* @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css); */
-
-@font-face {
-  font-family: 'AppleGothic';
-  src: url('../assets/fonts/AppleGothic.eot');
-  src: url('../assets/fonts/AppleGothic.eot?#iefix') format('embedded-opentype'),
-  url('../assets/fonts/AppleGothic.woff2') format('woff2'),
-  url('../assets/fonts/AppleGothic.woff') format('woff');
-  font-weight: normal;
-  font-style: normal;
-}
-
-@font-face {
-  font-family: 'S-Core Dream 4';
-  src: url('../assets/fonts/S-CoreDream-4Regular.eot?#iefix') format('embedded-opentype'),
-  url('../assets/fonts/S-CoreDream-4Regular.woff2') format('woff2'),
-  url('../assets/fonts/S-CoreDream-4Regular.woff') format('woff');
-  font-weight: 700;
-  font-style: normal;
-}
 
 @font-face {
   font-family: 'noto_sans_krbold';
