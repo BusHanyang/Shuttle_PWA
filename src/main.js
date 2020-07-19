bdf5f0e376ee11701d4837eabd626d352977a4bb
Yeonggyun Kim <kygha7205@naver.com>
2020-07-19 15:26:40 +0900

    Fix Indentations

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import {Vue as VueIntegration} from '@sentry/integrations';
 
 Sentry.init({
   dsn: 'https://358a2b17151340439edc038a774235e5@o422317.ingest.sentry.io/5347662',
-  integrations: [new VueIntegration({Vue, attachProps: true})],
+  integrations: [new VueIntegration({ Vue, attachProps: true })],
 });
 
 var VueCookie = require('vue-cookie');
@@ -36,6 +36,6 @@ if ('serviceWorker' in navigator) {
 new Vue({
   el: '#app',
   router,
-  components: {App},
+  components: { App },
   template: '<App/>'
 })
